03b73d8e7f88c99c133b914a37c1c37700410c8c
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-06-26 19:03:34 +0200

    Initial commit

---
+++ src/main/java/com/spring_boot_security_155/SpringBootSecurity155Application.java
@@ -0,0 +1,13 @@
+package com.spring_boot_security_155;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSecurity155Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSecurity155Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/spring_boot_security_155/SpringBootSecurity155ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring_boot_security_155;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSecurity155ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
