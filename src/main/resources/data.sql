6c641d16a1af26184ad7330616753aa92bdd9dfd
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-07-01 20:07:37 +0200

    Pobieranie usera z bazy

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,9 @@
 INSERT INTO users (id, user_name, password,is_Account_Non_Expired,is_Account_Non_Locked, is_Credentials_Non_Expired, is_Enabled)
 VALUES
-    (1, 'student', '123456', 1, 1, 1, 1),
-    (2, 'old', '123456', 1, 1, 1, 1),
-    (3, 'admin', '123456', 1, 1, 1, 1),
-    (4, 'verbs', '123456', 1, 1, 1, 1);
+    (1, 'student', '$2a$10$W1aW0lT8LUJAReMZ9vWh8usJt1IZ09sl0A4fueTno7JQ4G6bR8kgK', 1, 1, 1, 1),
+    (2, 'old', '$2a$10$seOy6zPeGKGTs0TMgtvo9ujWysNYmS.j9XzrW0NkthsZ82O1CBg8S', 1, 1, 1, 1),
+    (3, 'admin', '$2a$10$/xjGlwQ1wITKPZtPDhFRp.0DJKO8U/X7/Ial4179LBEGIaz94/fb2', 1, 1, 1, 1),
+    (4, 'verbs', '$2a$10$ZphhVNFvksmUMIxTQyoon.FTzpohTjtrTf74Rsnz46Gm.P6oBmiYS', 1, 1, 1, 1);
 
 
 INSERT INTO authorities(id, authority)
